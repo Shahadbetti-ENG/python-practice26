702fa0336b80cfca229b51add59b195e5f7862df
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-23 12:39:25 -0500

    Add number half pyramid nested loop pattern

---
+++ number_half_pyramid_nested_loop.py
@@ -0,0 +1,22 @@
+#Half Pyramid Pattern of Number
+#number_half_pyramid
+
+rows = 5
+
+#Outer loop controls the rows
+for i in range(1, rows + 1):
+    
+    #Inner loop prints numbers from 1 to i
+    for j in range(1, i + 1):       
+        
+        #Display the numbers on the same line      
+          print(j, end=" ")
+        
+    #New line after each row       
+    print()
+                   
+    
+   
+   
+
+    
