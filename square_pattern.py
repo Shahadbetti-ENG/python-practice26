c08f995d4518b1c76ca4f4a3af10bea6b8b8230c
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-25 10:59:36 -0500

    Add Square Star Pattern Program

---
+++ square_pattern.py
@@ -0,0 +1,11 @@
+# Square pattern
+
+rows = 5
+
+for i in range(rows):
+    
+    # Display 5 stars in the same row
+    print("* " * rows)
+ 
+# Move to the next line after each row we can skip this 
+#print(" ")
