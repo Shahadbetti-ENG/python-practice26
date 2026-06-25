9ac0e0d684f2a56ab26438e26a794299a2cb23d9
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-25 10:46:48 -0500

    Add Half Right Triangle Pattern

---
+++ half_right_triangle_pattern.py
@@ -0,0 +1,20 @@
+# right triangle
+
+# Number of rows in the triangle
+rows = 5
+
+# Outer loop starts from row 1 and continues to row 5  
+for i in range(1, rows + 1):
+    
+    # Display stars and increase stars at each rows 
+    print("* " * i )
+    
+"""
+#Logic Explanation
+
+*              row 1 = print 1 value
+* *            row 2 = print 2 value
+* * *          row 3 = print 3 value
+* * * *        row 4 = print 4 value
+* * * * *      row 5 = print 5 value
+"""
