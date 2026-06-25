538111527318076b34503ce7e4d860b444a12e7a
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-25 10:51:12 -0500

    Add Single Row Pattern

---
+++ single_row_pattern.py
@@ -0,0 +1,11 @@
+# single Row
+
+row = 5
+
+for i in range(row):
+    
+    # Display 5 stars in a row and put a space after each star
+    print("*", end=" ")
+    
+# Move to the new line we can skip this   
+#print()
