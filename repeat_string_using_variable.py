7e06ede3632e8344efac0eabed0930629f6d54d6
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-26 11:37:29 -0500

    Add repeat string using variable

---
+++ repeat_string_using_variable.py
@@ -0,0 +1,7 @@
+# Repeat a string using a variable
+
+# Store the string "Python" in the variable "word"
+word = "Python "
+
+# Display the value stored in the variable 5 times on the same line
+print(word * 5)
