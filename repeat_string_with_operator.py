a418d3e8dadee6f9e52fe9fe5c03303820dfa54f
Shahad Betti <shahadmazin2015@gmail.com>
2026-06-26 11:38:44 -0500

    Add repeat string with operator

---
+++ repeat_string_with_operator.py
@@ -0,0 +1,9 @@
+# Repeat a string using * operator
+
+# Repeat "Hello World!" 5 times, each on a new line
+# The \n escape sequence moves the cursor to the next line
+print("Hello World!\n" * 5)
+
+# Repeat "Hello World!" 5 times on the same line
+# Add a space after "!" so there is a space between each repetition
+print("Hello World! " * 5)
